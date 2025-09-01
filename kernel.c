bc73a59698d751267d6e5e140ee3c03c04217034
Thomasborgor <toppertommy3@gmail.com>
2025-09-01 14:28:26 -0500

    first commit

--- kernel.c
+++ kernel.c
@@ -3,18 +3,12 @@
 char *videomemptr = (char*)0xA0000;
 
 void kernelMain() {
-    uint32_t width  = 800;     // framebuffer width
-    uint32_t height = 600;     // framebuffer height
-    uint32_t pitch  = 200;    // bytes per row = width * 4 (32-bit color)
-
-    uint32_t color = 0x00FF00FF; // ARGB/XRGB
-
-    for (uint32_t y = 0; y < height; y++) {
-        uint32_t *row = (uint32_t*)(videomemptr + y * pitch);
-        for (uint32_t x = 0; x < width; x++) {
-            row[x] = color;
-        }
-    }
-
-    while (1) { asm("hlt"); }
+    unsigned long long thingy = 0;
+	while (thingy < 800*600) {
+		videomemptr[thingy * 4] = 0x0;
+		videomemptr[thingy * 4+1] = 0xff;
+		videomemptr[thingy * 4+2] = 0xff;
+		videomemptr[thingy * 4+2] = 0xff;
+		thingy++;
+	}
 }
